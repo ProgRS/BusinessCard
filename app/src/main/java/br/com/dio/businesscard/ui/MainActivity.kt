76c880bfce70d1ebbb891e5a348773359557ae91
ProgRs <lfernandotc@hotmail.com>
2022-07-11 17:08:12 -0300

    Conclusão da implementacão das telas

--- app/src/main/java/br/com/dio/businesscard/ui/MainActivity.kt
+++ app/src/main/java/br/com/dio/businesscard/ui/MainActivity.kt
@@ -1,15 +1,27 @@
 package br.com.dio.businesscard.ui
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import br.com.dio.businesscard.R
+
 import br.com.dio.businesscard.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private val binding by lazy{ ActivityMainBinding.inflate(layoutInflater) }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        insertListeners()
+    }
+
+    private fun insertListeners(){
+            binding.fab.setOnClickListener{
+                val intent = Intent(this@MainActivity, AddBusinessCardActivity::class.java)
+                //val intent = Intent(this, AddBusinessCardActivity::class.java)
+                startActivity(intent)
+
+            }
     }
 }
